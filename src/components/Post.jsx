8c6f56ee948994d03d485d8460c09337d9b4a225
Rayner Santana <raynersantana@plusoft.com>
2024-03-06 19:47:06 -0300

    Creating Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css'
 import { Comment } from './Comment'
+import { Avatar } from './Avatar'
 
 export function Post() {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src='https://github.com/raynersantana.png' />
+                    <Avatar src="https://github.com/raynersantana.png" />
                     <div className={styles.authorInfo}>
                         <strong>Rayner Santana</strong>
                         <span>Web Developer</span>
